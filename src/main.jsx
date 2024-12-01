358227e4fa4929d6eedfb24c7867b91329d04caf
Hasbi Nabil <hasbinabil.work@gmail.com>
2024-12-01 18:17:05 +0700

    feat: add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './routes/HomePage.jsx';
+import PostListPage from './routes/PostListPage.jsx';
+import SinglePostPage from './routes/SinglePostPage.jsx';
+import Write from './routes/Write.jsx';
+import RegisterPage from './routes/RegisterPage.jsx';
+import LoginPage from './routes/LoginPage.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomePage />,
+	},
+	{
+		path: '/posts',
+		element: <PostListPage />,
+	},
+	{
+		path: '/:slug',
+		element: <SinglePostPage />,
+	},
+	{
+		path: '/write',
+		element: <Write />,
+	},
+	{
+		path: '/register',
+		element: <RegisterPage />,
+	},
+	{
+		path: '/login',
+		element: <LoginPage />,
+	},
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
